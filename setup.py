afa1ee5dd9f04ebcd7e04ffc4da25bd113071648
Charles Leclerc <chl.charlesleclerc@gmail.com>
2021-11-21 23:33:32 +0100

    update

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='CharlesToolbox',
-      version="1.0",
-      description="Project Description",
-      packages=find_packages(),
-      install_requires=requirements,
-      test_suite='tests',
-      # include_package_data: to install data from MANIFEST.in
-      include_package_data=True,
-      scripts=['scripts/CharlesToolbox-run'],
-      zip_safe=False)
+setup(
+    name='charles_toolbox',
+    version="1.0",
+    description="Project Description",
+    packages=find_packages(),
+    install_requires=requirements,
+    test_suite='tests',
+    # include_package_data: to install data from MANIFEST.in
+    include_package_data=True,
+    scripts=['scripts/charles_toolbox-run'],
+    zip_safe=False)
